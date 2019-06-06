0c1595fb7a422e6b2f0cd42f4372c40e8ca67430
dasun <hmdtharaka@gmail.com>
2019-06-07 00:07:28 +0530

    BugFix: react-native-elements

--- awesome-app/components/MenuComponent.js
+++ awesome-app/components/MenuComponent.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { View, Flatlist } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
 function Menu(props) {
+
     const renderMenuItem = ({item, index}) => {
+
         return (
-            <ListItem
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                hideChevron={true}
-                leftAvatar={{ source: require('./images/uthappizza.png') }}
-            />
+                <ListItem
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    hideChevron={true}
+                    leftAvatar={{ source: require('./images/uthappizza.png')}}
+                  />
         );
-    }
+    };
+
     return (
-        <Flatlist
-            data={props.dishes}
-            renderItem={renderMenuItem}
-            keyExtractor={item => item.id.toString()}
-            />
+            <FlatList 
+                data={props.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
+                />
     );
 }
+
 
 export default Menu;
